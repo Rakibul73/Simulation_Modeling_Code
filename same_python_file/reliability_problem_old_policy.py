3dfd2680dcc26458290e7ffce3f8625efda023d6
Rakibul73 <rakibul29185@yahoo.com>
2023-03-20 21:01:09 +0600

    fixed old policy for same clock in go

--- same_python_file/reliability_problem_old_policy.py
+++ same_python_file/reliability_problem_old_policy.py
@@ -53,8 +53,9 @@ import random
 # for bearing 1
 total_hours_of_operation = 30000
 total_clock_hour = 0
-total_delay_minutes = 0
 total_change = 0    # how many bearing have to change
+bearing_1 = []
+bearing_1_delay = []
 
 while total_hours_of_operation > total_clock_hour:
     total_change += 1
@@ -69,17 +70,18 @@ while total_hours_of_operation > total_clock_hour:
             current_clock = bearing_life[i]
             break
 
+    bearing_1.append(current_clock)
     total_clock_hour += current_clock
 
 
     # find delay from generated random num 
     for i in range(len(delay_random_digits)):
         if delay_random_num in delay_random_digits[i]:
-            current_delay = delay_life[j]
+            current_delay = delay_life[i]
             break
 
 
-    total_delay_minutes += current_delay
+    bearing_1_delay.append(current_delay)
 
 
 import random
@@ -87,7 +89,8 @@ import random
 # for bearing 2
 total_hours_of_operation = 30000
 total_clock_hour = 0
-
+bearing_2 = []
+bearing_2_delay = []
 
 while total_hours_of_operation > total_clock_hour:
     total_change += 1
@@ -102,17 +105,18 @@ while total_hours_of_operation > total_clock_hour:
             current_clock = bearing_life[i]
             break
 
+    bearing_2.append(current_clock)
     total_clock_hour += current_clock
 
 
     # find delay from generated random num 
     for i in range(len(delay_random_digits)):
         if delay_random_num in delay_random_digits[i]:
-            current_delay = delay_life[j]
+            current_delay = delay_life[i]
             break
 
 
-    total_delay_minutes += current_delay
+    bearing_2_delay.append(current_delay)
 
 
 import random
@@ -120,7 +124,8 @@ import random
 # for bearing 3
 total_hours_of_operation = 30000
 total_clock_hour = 0
-
+bearing_3 = []
+bearing_3_delay = []
 
 while total_hours_of_operation > total_clock_hour:
     total_change += 1
@@ -135,43 +140,102 @@ while total_hours_of_operation > total_clock_hour:
             current_clock = bearing_life[i]
             break
 
+    bearing_3.append(current_clock)
     total_clock_hour += current_clock
 
 
     # find delay from generated random num 
     for i in range(len(delay_random_digits)):
         if delay_random_num in delay_random_digits[i]:
-            current_delay = delay_life[j]
+            current_delay = delay_life[i]
             break
 
 
-    total_delay_minutes += current_delay
+    bearing_3_delay.append(current_delay)
 
+
+
+maxlen = max( len(bearing_2) , len(bearing_1) , len(bearing_3)  )
+two_down = 0
+three_down = 0
+try:
+    for i in range(maxlen):
+        if (bearing_1[i] == bearing_2[i] == bearing_3[i]):
+            three_down += 1
+            max_delay = max(bearing_3_delay[i] , bearing_2_delay[i] , bearing_1_delay[i])
+            if bearing_3_delay[i] != max_delay:
+                bearing_3_delay[i] = 0
+            if bearing_2_delay[i] != max_delay:
+                bearing_2_delay[i] = 0
+            if bearing_1_delay[i] != max_delay:
+                bearing_1_delay[i] = 0
+        elif bearing_1[i] == bearing_2[i]:
+            two_down += 1
+            if bearing_1_delay[i] < bearing_2_delay[i]:
+                bearing_1_delay[i] = 0
+            else:
+                bearing_2_delay[i] = 0
+        elif bearing_3[i] == bearing_2[i]:
+            two_down += 1
+            if bearing_3_delay[i] < bearing_2_delay[i]:
+                bearing_3_delay[i] = 0
+            else:
+                bearing_2_delay[i] = 0
+        elif bearing_1[i] == bearing_3[i]:
+            two_down += 1
+            if bearing_1_delay[i] < bearing_3_delay[i]:
+                bearing_1_delay[i] = 0
+            else:
+                bearing_3_delay[i] = 0
+
+
+
+
+except:
+    pass
+
+
+# print(two_down , three_down)
+# print("\n")
+# print(bearing_1)
+# print(bearing_2)
+# print(bearing_3)
+# print("\n")
+# print(bearing_1_delay)
+# print(bearing_2_delay)
+# print(bearing_3_delay)
 
 
 # 20 minutes to change one bearing
 # Downtime of the machining center costs Rs. 5 per minute
 # direct on job cost of the repairman is Rs. 25 per hour 
-# cost of a bearing is Rs. 20.
+# at a time cost of one bearing is Rs. 20.
+# at a time cost of two bearing is Rs. 30.
+# at a time cost of three bearing is Rs. 40.
 
 one_bearing_change_time_minutes = 20
+two_bearing_change_time_minutes = 30
+three_bearing_change_time_minutes = 40
 cost_of_downtime_per_minutes = 5
 cost_of_repairman_per_hour = 25
 cost_of_one_bearing = 20
 
-print("Total repairman delay time =" , total_delay_minutes , "minutes")
-print("Time spent in bearing replacement =", total_change*one_bearing_change_time_minutes , "minutes")
-print("Total downtime =" , total_delay_minutes+(total_change*one_bearing_change_time_minutes) , "minutes")
+total_repairman_delay_time = sum(bearing_1_delay) + sum(bearing_2_delay) + sum(bearing_3_delay)
+print("Total repairman delay time =" , total_repairman_delay_time , "minutes")
+time_spent_in_bearing_replacement = (total_change * one_bearing_change_time_minutes) - (two_down * (two_bearing_change_time_minutes - one_bearing_change_time_minutes)) - (three_down * (three_bearing_change_time_minutes - one_bearing_change_time_minutes))
+print("Time spent in bearing replacement =", time_spent_in_bearing_replacement , "minutes")
+total_downtime = total_repairman_delay_time + time_spent_in_bearing_replacement
+print("Total downtime =" , total_downtime , "minutes")
 
-cost_of_downtime = cost_of_downtime_per_minutes*(total_delay_minutes+(total_change*one_bearing_change_time_minutes))
+cost_of_downtime = cost_of_downtime_per_minutes*total_downtime
 print("Cost of downtime = Rs.",  cost_of_downtime)
-cost_of_repairman = total_change*one_bearing_change_time_minutes*cost_of_repairman_per_hour/60
+cost_of_repairman = time_spent_in_bearing_replacement*cost_of_repairman_per_hour/60
 print("Cost of repairman = Rs.",  cost_of_repairman)
+
 print("\n")
 print("Total bearing replaced =", total_change)
 cost_of_bearing = cost_of_one_bearing*total_change
 print("Cost of bearing = Rs.",  cost_of_bearing)
+
 print("\n")
-
-
 print("Total estimated cost = Rs.",  cost_of_repairman+cost_of_bearing+cost_of_downtime)
